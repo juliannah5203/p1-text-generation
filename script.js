038979660e0fba5ea9678c102ad5ad3e6920a53e
juliannah5203 <57543226+juliannah5203@users.noreply.github.com>
2022-06-23 22:43:59 -0700

    finished

--- script.js
+++ script.js
@@ -1,34 +1,17 @@
 const fillers = {
-    // adventurer: ["My dude", "Bro", "Adventurer", "Traveller", "Fellow", "Citizen", "Ashen One", "Dragonborn", "Cool person", "Tarnished", "$adventurer and $adventurer", "$adventurer, $adventurer, and $adventurer", "Geoff"],
-    // pre: ["Fra", "Tro", "Gre", "Pan", "Ast", "Ara"],
-    // post: ["gria", "ston", "gott","-on-the-lee", "ora", "Ara", "uwu"],
-    // people: ["kindly", "meek", "brave", "wise", "sacred", "cherished", "honored", "forgotten", "apathetic", "mystic", "orca"],
-    // item: ["axe", "staff", "book", "cloak", "shield", "club", "sword", "magic gloves", "galvel", "fists", "mace", "potato"],
-    // num: ["two", "three", "eleven", "so many", "too many", "an unsatisfying number of", "barely any", "an unspecified amount of", "surely a satisfactory number of"],
-    // looty: ["gleaming", "valuable", "esteemed", "rare", "exalted", "scintillating", "kinda gross but still usefull", "complete garbage"],
-    // loots: ["coins", "chalices", "ingots", "hides", "victory points", "gems","scrolls", "bananas", "noodles", "goblins", "CS Majors", "college credits"],
-    // baddies: ["orcs", "glubs", "fishmen", "cordungles", "mountain trolls", "college professors", "dragon", "evil $adventurer", "agents of chaos"],
-    // message: ["call", "txt", "post", "decree", "shoutz", "tweets", "choiche", "hearkens", "harkening", "harkenening", "harkenenening", "...wait, no! Come back", "Watermelon"],
     name: ["Tom", "Jerry", "Julianna", "John", "Mary", "May", "Crystal", "Amber", "Eva", "Isbella", "Mia", "Ella", "Sofia", "Scarlett", "Emily", "William", "Owen", "Ethan"],
     country: ["United States", "Mexico", "Canada", "Brazil", "China", "Vietnam", "Chile", "India", "Iran", "Thailand", "Turkey", "United Kingdom", "South Korea", "Russia"],
-    fruit: ["apples", "oranges", "grapes", "peaches", "pears", "strawberries", "bananas", "blueberries", "grapefruits", "plums", "lemons", "longans", "watermelons"],
-    amount: ["two", "three", "a lot", "so many", "eleven", "twelve", "immeasurable", "inestimable", "too many"],
+    fruit: ["apples", "oranges", "grapes", "peaches", "pears", "strawberries", "bananas", "blueberries", "grapefruits", "plums", "lemons", "longans", "watermelons", "$fruit and $fruit"],
     scale: ["thousand", "billion", "million", "hundred", "trillion", "quadrillion", "quintillion"],
+    amount: ["two", "three", "a lot", "so many", "eleven", "twelve", "immeasurable", "inestimable", "too many", "$amount $scale"],
     unit: ["grams", "kilograms", "pounds", "ounces", "tons"],
     percent: ["10", "20", "30", "40", "50", "60", "70", "80", "90"],
-
-
-
-
-
-    
+    greeting: ["Hi", "Hello", "Nice to meet you"],
   };
   
-  const template = `$name, I come from $country!
-  
-  $country is one of the countries that produces the most $fruit. Do you like $fruit? If you do, come with me!
+  const template = `$greeting! $name, I come from $country! $country is one of the countries that produces the most $fruit. Do you like $fruit? If you do, come with me! We can talk about how to plant $fruit in your hometown, $country, or if you are interesting in trading.
 
-  Every year, there are approximately $amount $scale $unit of $fruit exported to other countries. $percent % of the production will be delivered to $country. The rest of them will export to $country and $country.
+  Every year, there are approximately $amount $scale $unit of $fruit exported to other countries. $percent % of the production will be delivered to $country. The rest of them will export to $country and $country. 
 
   `;
   
